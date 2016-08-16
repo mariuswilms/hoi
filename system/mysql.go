344dd91a77a9e007f914e845eea4f991f683502f
David Persson <davidpersson@gmx.de>
2016-08-16 12:28:51 +0200

    Add bug comment.

--- system/mysql.go
+++ system/mysql.go
@@ -3,6 +3,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// BUG(nperson) Revoke/Grant does not set dirty flag.
 package system
 
 import (
